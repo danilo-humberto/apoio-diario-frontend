fdb9929c6efac7304955942a730c6048b0c9aa2e
Danilo <danilohumberto12@hotmail.com>
2025-11-23 20:02:43 -0300

    feat: finalizando tela de boas-vindas

--- src/features/welcome/WelcomeScreen.tsx
+++ src/features/welcome/WelcomeScreen.tsx
@@ -1,9 +1,20 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Image, Text, View } from "react-native";
 
 const WelcomeScreen = () => {
   return (
-    <View>
-      <Text>WelcomeScreen</Text>
+    <View className="bg-background flex-1 items-center justify-center gap-2">
+      <View className="bg-primary rounded-full p-4">
+        <Image
+          source={require("../../assets/images/logo.png")}
+          className="w-28 h-28"
+          resizeMode="contain"
+        />
+      </View>
+      <Text className="text-black/80">Rotina simples e organizada!</Text>
+      <Link href={"/login"} className="mt-10 bg-primary w-5/6 py-6 rounded-2xl">
+        <Text className="text-center text-lg">Começar</Text>
+      </Link>
     </View>
   );
 };
